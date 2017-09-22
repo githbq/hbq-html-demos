19d947e3771d80c8cd816bed6ecccdfa9ef47261
hbq <hbqhbq123@qq.com>
2017-09-22 23:25:57 +0800

    css3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 window.htmls = [
-  "html/css3动画/index.html",
+  "html/css3动画/小球弹跳/index.html",
+  "html/css3动画/风车/index.html",
+  "html/css3动画/齿轮/index.html",
   "html/other-demo.html",
   "html/sidebar-demo.html",
   "html/test/other-demo.html",
-  "html/vue-demo.html"
+  "html/vue-demo.html",
+  "html/齿轮/index.html"
 ];
