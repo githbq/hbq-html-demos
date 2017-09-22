4b8399e0a9a5f833598807be8b3531aab5cdae4a
hbq <hbqhbq123@qq.com>
2017-09-22 23:51:48 +0800

    watch_less

--- lite-server-config.js
+++ lite-server-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    files: ['./src/**/*.{html,htm,css,js}'],
+    files: ['./src/**/*.{html,htm,css,js,less}'],
     server: { baseDir: './src' },
     startPath: "/index.html"
 }
